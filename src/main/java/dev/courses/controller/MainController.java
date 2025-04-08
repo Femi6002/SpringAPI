23c6854a759ccc584bc496c16324130e46ca8de7
Femi6002 <161641223+Femi6002@users.noreply.github.com>
2025-04-08 11:59:51 +0100

    Add files via upload

--- src/main/java/dev/courses/controller/MainController.java
+++ src/main/java/dev/courses/controller/MainController.java
@@ -1,21 +1,43 @@
 package dev.courses.controller;
 
+import dev.courses.model.DataBaseSequence;
 import dev.courses.model.Student;
 import dev.courses.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @RestController
 public class MainController {
 
 @Autowired
 StudentRepo studentRepo;
+@Autowired
+private MongoOperations mongoOperations;
+
+
+    public long generateSequence(String seqName) {
+        DataBaseSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+                new Update().inc("seq",1), options().returnNew(true).upsert(true),
+                DataBaseSequence.class);
+        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+    }
 
     @PostMapping("/addStudent")//adding to a database is called posting
     public void addStudent(@RequestBody Student student){
-        studentRepo.save(student);
+        Student student1 = new Student();
+        student1.setRno(generateSequence(student.SEQUENCE_NAME));
+        student1.setAddress(student.getAddress());
+        student1.setName(student.getName());
+        studentRepo.save(student1);
     }
 
     @GetMapping("/getStudent/{id}")
@@ -31,7 +53,7 @@ StudentRepo studentRepo;
     @PutMapping("/updateStudent")//adding to a database is called posting
     public void updateStudent(@RequestBody Student student){
         //fetch student by id
-        Student data = studentRepo.findById(student.getRno()).orElse(null);
+        Student data = studentRepo.findById((int) student.getRno()).orElse(null);
         System.out.println(data);
 
         //check if null
--- src/main/java/dev/courses/model/DataBaseSequence.java
+++ src/main/java/dev/courses/model/DataBaseSequence.java
@@ -1,25 +1,21 @@
 package dev.courses.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
-
-@Document
+@Document("database_sequence")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Student {
-    @Transient
-    public static final String SEQUENCE_NAME = "student_sequence";
+public class DataBaseSequence {
     @Id
-    private long rno;
-    public String name;
-    public String address;
+    private String id;
+
+    private long seq;
 }
